26bc637e1ddcecf457d2b30ea9eb01e61badc92c
KhathutsheloCarter <121690232+KhathutsheloCarter@users.noreply.github.com>
2023-09-13 18:28:35 +0200

    Add files via upload

---
+++ palindum number/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindum_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter  a number");
+            int InputNumber = Convert.ToInt32(Console.ReadLine());
+            int Remeinder = 0;
+            int ReverseNumber = 0;
+            int Contigency = InputNumber;
+
+            while (InputNumber > 0)
+            {
+
+                InputNumber = InputNumber / 10;
+                ReverseNumber = InputNumber % 10;
+                Remeinder = ReverseNumber * 10 + Remeinder;
+            }
+
+            if (Contigency == ReverseNumber)
+            {
+
+                Console.WriteLine("This is a palindum number");
+            }
+            else
+            {
+                Console.WriteLine("This is not  a palindum number");
+            }
+            Console.ReadLine();
+
+
+        }
+
+     
+    }
+}
